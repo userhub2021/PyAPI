b2151dbc4890758b108e0f58b208e9c947825ff3
userhub2021 <taka082191@gamil.com>
2022-04-26 17:59:26 +0900

    Revert "新規学習API"
    This reverts commit 64bb2a595af8d724f5b28513b022a9c7c35c702e.

--- jobs/api/serializers.py
+++
@@ -1,8 +0,0 @@
-from dataclasses import field
-from rest_framework import serializers
-from jobs.models import JobOffer
-
-class JobOfferSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = JobOffer
-        fields = ('company_name', 'company_email', 'job_title', 'job_description', 'salary', 'prefectures',  'city', 'create_at')
